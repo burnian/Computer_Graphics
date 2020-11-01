fc977668d234c3b86f8169457edf6372229b0ecd
Burnian.Zhou <burnian@qq.com>
2020-11-01 12:04:16 +0800

    translate the Chinese comments to English in case of that they may turn into messy code when the files move to other systems.

--- MyOpenGL/MyOpenGL/res/shader/blinn_phong_shadow.fs
+++ MyOpenGL/MyOpenGL/res/shader/blinn_phong_shadow.fs
@@ -1,4 +1,6 @@
 #version 330 core
+out vec4 FragColor;
+
 struct Material {
     sampler2D diffuse;
     sampler2D specular;
@@ -8,77 +10,84 @@ struct Material {
 uniform Material material;
 
 struct DirLight {
+	// light
     vec3 direction;
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+	// shadow
+	sampler2D depthMap;
 };
 uniform DirLight dirLight;
 
-uniform sampler2D depthMap;
-uniform vec3 viewPos;
-
 in VS_OUT {
-    vec3 FragPos;
+    vec3 FragWPos;
     vec3 Normal;
     vec2 TexCoords;
-    vec4 FragPosLightSpace;
+    vec4 FragLightSpacePos;
 } fs_in;
 
-out vec4 FragColor;
+uniform vec3 viewWPos; // WPos means world space position
 
-// ÒõÓ°¼ÆËã£¬º¯ÊıÖĞµÄ×ø±ê¶¼»ùÓÚlight space
-float ShadowCalculation(vec4 fragPosLightSpace, DirLight light, vec3 normal) {
-    // perform perspective divide£¬Õı½»Í¶Ó°ÒòÎªwÖµÃ»ÓĞĞŞ¸Ä£¬ËùÒÔ²»ĞèÒª³ıw£¬µ«ÊÇÍ¸ÊÓÍ¶Ó°ĞèÒª¡£
-	// °ÑÆ¬ÔªÍ¶Ó°×ø±ê×ª»¯ÎªNDC£¬ÖµÓò´Ó[-w,w]->[-1,1]
-	vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
-	// ´ÓNDCµÄclip spaceµÄÖµÓò[-1,1]µ½ÎÆÀí¿Õ¼äµÄÖµÓò[0,1]
+// LPos means light space position
+float CalcDirLightShadow(vec4 fragLPos, DirLight light, vec3 normal) {
+    // perform perspective divide, but when fragLPos is based on orthogonal projectionï¼Œ
+	// the w value is still the same 1, there is no need to do the dividing like below.
+	// transforming the position from common space to NDC, from range [-w,w] to range [-1,1]
+	vec3 projCoords = fragLPos.xyz / fragLPos.w;
+	// transforming the NDC's clip space range from [-1,1] to [0,1]
 	projCoords = projCoords * 0.5 + 0.5;
 	float currentDepth = projCoords.z;
-	// ³¬³ö¹âÔ´Æ½½ØÍ·ÌåÔ¶²ÃÃæµÄÆ¬ÔªÉî¶ÈÖµ´óÓÚ1£¬Ôò¸Ãº¯Êıºã·µ»Ø0£¬×ÜÓĞÒõÓ°£¬¶øÊµ¼ÊÉÏÕâ²¿·ÖÆ¬Ôª¿ÉÄÜÃ»ÓĞÕÚµ²£¬²»¸ÃÓĞÒõÓ°
+	// the depth of every fragment outside the frustum is larger than 1, but the maximun of closest
+	// depth is always be 1, then the fragments outside the frustum will be rendered like they are
+	// occluded by the edge of the frustum, but in fact, they aren't.
 	if(currentDepth > 1.0) return 1.0;
-	// ¶ÔÖ®Ç°ÔÚlight spaceÏÂ¼ÆËã³öÀ´µÄÒõÓ°ÎÆÀíÌùÍ¼£¨±£´æµÄÊÇÉî¶ÈÖµ£©½øĞĞ²ÉÑù
+	// sampling closest depth from the shadow map generated from the depth_map shader before.
 	// float closestDepth = texture(depthMap, projCoords.xy).r;
-	// ÒõÓ°ÎÆÀíÒ²ÊÇ´æÔÚ·Ö±æÂÊµÄ£¬ËùÒÔÆäÖµÒ²ÊÇÀëÉ¢µÄ£¬µ«Æ¬Ôª×ø±êÈ´ÊÇ½üËÆÁ¬ĞøµÄ£¬Õâ¾ÍÊ¹µÃ¾à¹âÔ´½ÏÔ¶ÇÒ¾àÀë½Ï½üµÄÁ¬Ğø
-	// Æ¬Ôª¿ÉÄÜ²ÉÑùµ½Í¬Ò»¸öÎÆËØ¶ÔÓ¦µÄÉî¶ÈÖµd£¬±ØÈ»´æÔÚÆäÖĞÒ»¸öÆ¬ÔªÉî¶ÈĞ¡ÓÚd£¬ÁíÒ»¸öÉî¶È´óÓÚd£¬ÌõÎÆÒõÓ°¾Í³öÏÖÁË
-	// Í¨¹ı¸øÆ¬ÔªÉî¶ÈÌí¼Ó´¹Ö±Æ«ÒÆÁ¿À´½â¾öÌõÎÆÒõÓ°ÎÊÌâ
+	// the value in shadow map is discrete, it results in the continuous fragments may sample the same
+	// closest depth, because they are near from each other and far from light source. When their 
+	// distances to the light source is different, one maybe larger than the closest depth and one maybe
+	// not, this results in the shadow stripes.
+	// Solving this by adding bias which is related to the angle between the normal and the light
+	// direction, the bias is in direct proportion to the angle.
 	float bias = max(0.05 * (1.0 - dot(normal, normalize(-light.direction))), 0.005);
 	currentDepth -= bias;
 
 	float shadow = 0.0;
-	// ¼ÆËãÎÆËØÔÚÎÆÀí¿Õ¼ä[0,1]ÖĞµÄ³ß´ç
-	vec2 texelSize = 1.0 / textureSize(depthMap, 0);
+	// textureSize function returns the width(the same as height) of the shadow map
+	vec2 texelSize = 1.0 / textureSize(light.depthMap, 0);
+	// multi-sampling to avoid the mosaic shadow
 	for(int x = -1; x <= 1; ++x)
 		for(int y = -1; y <= 1; ++y) {
-			float pcfDepth = texture(depthMap, projCoords.xy + vec2(x, y) * texelSize).r;
-			shadow += currentDepth > pcfDepth ? 0.0 : 1.0;
+			// Percentage-closer filtering
+			float PCFDepth = texture(light.depthMap, projCoords.xy + vec2(x, y) * texelSize).r;
+			shadow += currentDepth > PCFDepth ? 0.0 : 1.0;
 		}
 
 	return shadow / 9.0;
 }
 
-// Æ½ĞĞ¹â
-vec3 CalcDirLight(DirLight light, vec3 normal, vec3 towardView, float shadow){
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 frag2ViewWS, float shadow) {
     vec3 towardLight = normalize(-light.direction);
-    // diffuse shading Óë¹Û²ìÕßÎ»ÖÃÎŞ¹Ø
-    float diffStrength = max(dot(normal, towardLight), 0.0); // ¹âÕÕÇ¿¶È²ÎÊı
-    // specular shading Óë¹Û²ìÕßÎ»ÖÃÏà¹Ø£¬ÕâÀïºÍPhong ²»Í¬
-	vec3 halfwayDir = normalize(towardLight + towardView);
-	float specStrength = pow(max(dot(normal, halfwayDir), 0.0), material.shininess);
+    // diffuse shading
+    float diffWeight = max(dot(normal, towardLight), 0.0);
+    // specular shading
+	vec3 halfwayDir = normalize(towardLight + frag2ViewWS);
+	float specWeight = pow(max(dot(normal, halfwayDir), 0.0), material.shininess);
     // combine results
     vec3 ambient = light.ambient * texture(material.diffuse, fs_in.TexCoords).rgb;
-    vec3 diffuse = light.diffuse * diffStrength * texture(material.diffuse, fs_in.TexCoords).rgb;
-    vec3 specular = light.specular * specStrength * texture(material.specular, fs_in.TexCoords).rgb;
+    vec3 diffuse = light.diffuse * diffWeight * texture(material.diffuse, fs_in.TexCoords).rgb;
+    vec3 specular = light.specular * specWeight * texture(material.specular, fs_in.TexCoords).rgb;
     return ambient + shadow * (diffuse + specular);
 }
 
-
 void main() {
 	vec3 norm = normalize(fs_in.Normal);
-	vec3 towardView = normalize(viewPos - fs_in.FragPos);
-    float shadow = ShadowCalculation(fs_in.FragPosLightSpace, dirLight, norm);
+	vec3 frag2ViewWS = normalize(viewWPos - fs_in.FragWPos);
 	// phase 1: Directional lighting
-	vec3 result = CalcDirLight(dirLight, norm, towardView, shadow);
-    
-    FragColor = vec4(result, 1.0);
+	float shadow = CalcDirLightShadow(fs_in.FragLightSpacePos, dirLight, norm);
+	vec3 result = CalcDirLight(dirLight, norm, frag2ViewWS, shadow);
+
+	// FragColor = vec4(vec3(shadow), 1.0);
+	FragColor = vec4(result, 1.0);
 }
--- MyOpenGL/MyOpenGL/res/shader/blinn_phong_shadow.vs
+++ MyOpenGL/MyOpenGL/res/shader/blinn_phong_shadow.vs
@@ -4,26 +4,26 @@ layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
 
 out VS_OUT {
-    vec3 FragPos;
+    vec3 FragWPos;
     vec3 Normal;
     vec2 TexCoords;
-    vec4 FragPosLightSpace;
+    vec4 FragLightSpacePos;
 } vs_out;
 
 uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 model;
-uniform mat4 lightSpaceMat;
+uniform mat4 lightSpaceMat; // world to light to light's perspective transformation matrix
 
 void main()
 {
-    vs_out.FragPos = vec3(model * vec4(aPos, 1.0)); // from local space to world space
-    vs_out.Normal = transpose(inverse(mat3(model))) * aNormal; // from local to world
+    vs_out.FragWPos = vec3(model * vec4(aPos, 1.0)); // local space to world space
+    vs_out.Normal = transpose(inverse(mat3(model))) * aNormal; // local to world
     vs_out.TexCoords = aTexCoords;
-    vs_out.FragPosLightSpace = lightSpaceMat * vec4(vs_out.FragPos, 1.0); // from world to light
-    gl_Position = projection * view * vec4(vs_out.FragPos, 1.0); // world to view to perspective
+    vs_out.FragLightSpacePos = lightSpaceMat * vec4(vs_out.FragWPos, 1.0); // world to light's perspective
+    gl_Position = projection * view * vec4(vs_out.FragWPos, 1.0); // world to viewer to viewer's perspective
 }
 
 /*
-针对单一光源，核心参数：lightSpaceMat
+for single light source, the key parameter is lightSpaceMat
 */
